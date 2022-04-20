a57e47d6bfc2e0cb7f3b8132de314627079d6e9e
vikasnare1992 <vikasnare1992@gmail.com>
2022-04-20 19:24:37 +0530

    Added the First condition check

--- Incubyte-test/Calculator/src/com/local/cal/Calculator.java
+++ Incubyte-test/Calculator/src/com/local/cal/Calculator.java
@@ -6,11 +6,40 @@ public class Calculator {
 			return 0;
 		}
 		
-		return 0;
+		String delimiter = ",";
+		int num1 = 0;
+		int num2 = 0;
+		
+		numbers = numbers.replaceAll("\n", "");
+		String[] splitArray = numbers.split(delimiter);
+		if (splitArray.length == 1) {
+			if (splitArray[0].trim().length() > 0) {
+				try {
+					num1 = Integer.parseInt(splitArray[0].trim());
+				} catch (Exception e) {
+				}
+			}
+		} else {
+			if (splitArray[0].trim().length() > 0) {
+				try {
+					num1 = Integer.parseInt(splitArray[0].trim());
+				} catch (Exception e) {
+				}
+			}
+			
+			if (splitArray[1].trim().length() > 0) {
+				try {
+					num2 = Integer.parseInt(splitArray[1].trim());
+				} catch (Exception e) {
+				}
+			}
+		}
+		
+		return num1 + num2;
 	}
 	
 	public static void main(String[] args) {
-		String numbers = "";
+		String numbers = "1, 2";
 		int result = new Calculator().Add(numbers);
 		System.out.println("Result : "+result);
 	}
